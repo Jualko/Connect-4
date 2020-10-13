4b1964255e0a9bab25b3708f619821ddb4329213
Jualko <66382948+Jualko@users.noreply.github.com>
2020-10-13 15:09:05 +0200

    addToken()

--- connect4.js
+++ connect4.js
@@ -1,3 +1,5 @@
+var player = "p1";
+
 document.addEventListener("DOMContentLoaded", function (event) {
     attachEventListeners();
 });
@@ -5,10 +7,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function attachEventListeners() {
     var elements = document.getElementsByClassName("row");
     var aElements = [].slice.call(elements);
-    aElements.forEach(e => e.addEventListener("click", handleRoClick));
+    aElements.forEach(e => e.addEventListener("click", handleRowClick));
 }
 
-function handleRoClick(){
-   var elements= this.children;
-   debugger
+function handleRowClick(oEv) {
+    addToken(this)
+}
+
+function addToken(row) {
+    fields = row.children;
+    for (let i = 1; i < fields.length + 1; i++) {
+        const e = fields[fields.length - i];
+        if (!e.classList.contains("p1") && !e.classList.contains("p2")) {
+            e.classList.add(player);
+            switchPlayer();
+            return;
+        }
+    }
+}
+
+function switchPlayer() {
+    if (player === "p1") {
+        player = "p2";
+    } else if (player === "p2") {
+        player = "p1";
+    }
 }
